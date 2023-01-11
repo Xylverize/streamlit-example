ff26380460afb678820e86acb92ce41b789011df
Xyla Ramadhan <71839605+Xylverize@users.noreply.github.com>
2023-01-11 10:19:31 +0700

    Add files via upload

---
+++ app_test.py
@@ -0,0 +1,89 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+st.header('FTDS Model Deployment')
+st.write("""
+Test 
+""")
+
+@st.cache
+def fetch_data():
+    df = pd.read_csv('https://raw.githubusercontent.com/ardhiraka/PFDS_sources/master/campus.csv')
+    return df
+
+df = fetch_data()
+st.write(df)
+
+st.sidebar.header('User Input Features')
+
+def user_input():
+    gender = st.selectbox("Customer Gender", ["Male", "Female"])
+    seniorCitizen = st.selectbox("Whether the customer is a senior citizen or not", ["No", "Yes"] )
+    partner = st.selectbox("Whether the customer has a partner or not", ["No", "Yes"])
+    dependent = st.selectbox(" Whether the customer has dependents or not", ["No", "Yes"])
+    tenure = st.number_input("Number of months the customer has stayed with the company? (Tenure)", min_value=1)
+    phoneService = st.selectbox("Phone service ", ["No", "Yes"])
+    multipleLines = st.selectbox("Multiple Lines ", ["No", "Yes","No phone service"])
+    internetService = st.selectbox("Internet Service Provider ", ["No", "DSL", "Fiber optic"])
+    onlineSecurity = st.selectbox("Online Security ", ["No", "Yes","No internet service"])
+    onlineBackup = st.selectbox("Online Backup ", ["No", "Yes","No internet service"])
+    deviceProtection = st.selectbox("Device Protection", ["No", "Yes","No internet service"])
+    techSupport = st.selectbox("Tech Support ", ["No", "Yes","No internet service"])
+    streamingTV = st.selectbox("Streaming TV ", ["No", "Yes","No internet service"])
+    streamingMovies = st.selectbox("Streaming Movies ", ["No", "Yes","No internet service"])
+    contract = st.selectbox("Contract ", ["Month-to-month", "One year", "Two year"])
+    paperlessBilling = st.selectbox("Paperless Billing ", ["Yes", "No"])
+    paymentMethod = st.selectbox("Payment Method ",
+                [
+                    "Electronic check",
+                    "Mailed check",
+                    "Bank transfer (automatic)",
+                    "Credit card (automatic)",
+                ],
+            )
+    monthlyCharges = st.number_input("Monthly Charges", value=18.0,min_value=18.0)    
+    totalCharges = st.number_input("Total Charges ",value=18.0,min_value=18.0)
+    
+    new_data = {
+            "gender": gender,
+            "SeniorCitizen": seniorCitizen,
+            "Partner": partner,
+            "Dependents": dependent,
+            "tenure": tenure,
+            "PhoneService": phoneService,
+            "MultipleLines": multipleLines,
+            "InternetService": internetService,
+            "OnlineSecurity": onlineSecurity,
+            "OnlineBackup": onlineBackup,
+            "DeviceProtection": deviceProtection,
+            "TechSupport": techSupport,
+            "StreamingTV": streamingTV,
+            "StreamingMovies": streamingMovies,
+            "Contract": contract,
+            "PaperlessBilling": paperlessBilling,
+            "PaymentMethod": paymentMethod,
+            "MonthlyCharges": monthlyCharges,
+            "TotalCharges": totalCharges,
+        }
+
+    features = pd.DataFrame(new_data, index=[0])
+    return features
+
+
+input = user_input()
+
+st.subheader('User Input')
+st.write(input)
+
+load_model = joblib.load("preprocessor.pkl")
+
+prediction = load_model.predict(input)
+
+if prediction == 1:
+    prediction = 'Churn'
+else:
+    prediction = 'Not Churn'
+
+st.write('Based on user input, the placement model predicted: ')
+st.write(prediction)
